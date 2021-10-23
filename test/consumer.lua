c79291e449df18418a5775989722c35ae602f267
Dmitry Kudrin <qdrin@mail.ru>
2021-10-23 20:24:18 +0300

    Add consumer_tag

--- test/consumer.lua
+++ test/consumer.lua
@@ -91,7 +91,6 @@ local test_no_routing_key = function(test)
         queue = "work_q",
         vhost = "workhost",
         exchange = "work.pub",
-        routing_key = "work.rk",
         durable = true,
         passive = true,
         ssl = false,
@@ -132,9 +131,139 @@ local test_no_routing_key = function(test)
     test:is(err, nil, "No error on close")
 end
 
+local test_prefetch_count = function(test)
+  test:plan(15)
+
+  test:diag("Client should use prefetch_count for safe connection")
+  local get_worker = helper.get_worker
+
+  local ctx = amqp.new({
+      role = "producer",
+      exchange = "work.pub",
+      routing_key = "work.rk",
+      virtual_host = "workhost",
+      passive = true,
+      auto_delete = false,
+      durable = true,
+      ssl = false,
+      user = "guest",
+      password = "guest",
+  })
+  local c_args = {
+      role = "consumer",
+      queue = "work_q",
+      vhost = "workhost",
+      exchange = "work.pub",
+      durable = true,
+      passive = true,
+      ssl = false,
+      user = "read_user",
+      password = "read_user",
+      prefetch_count = 10,
+  }
+  test:isnt(ctx, nil, 'Ctx created')
+
+  local ok, err = ctx:connect("127.0.0.1", 5672)
+  test:ok(ok, "Connect status ok")
+  test:is(err, nil, "No error on connect")
+
+  ok, err = ctx:setup()
+  test:ok(ok, "Setup status ok")
+  test:is(err, nil, "No error on setup")
+
+  local wrk
+  wrk, err = get_worker(c_args)
+  test:ok(ok, "Consume worker creation ok")
+  wrk:consume()
+  fiber.sleep(5)
+  local res
+  res, err = helper.get_rmq_data()
+  test:ok(res, 'rmq data get ok')
+  test:is(#res.consumer_details, 1, 'consumer_details contains 1 element')
+  test:is(res.consumer_details[1].prefetch_count, 10, 'prefetch_count ok')
+  ok, err = wrk:close()
+  test:ok(ok, "Close consumer status ok")
+  test:is(err, nil, "No error on consumer close")
+
+  ok, err = ctx:teardown()
+  test:ok(ok, "Teardown status ok")
+  test:is(err, nil, "No error on teardown")
+
+  ok, err = ctx:close()
+  test:ok(ok, "Close status ok")
+  test:is(err, nil, "No error on close")
+end
+
+local test_consumer_tag = function(test)
+  test:plan(15)
+
+  test:diag("Client should use consumer-tag for connection")
+  local get_worker = helper.get_worker
+
+  local ctx = amqp.new({
+      role = "producer",
+      exchange = "work.pub",
+      routing_key = "work.rk",
+      virtual_host = "workhost",
+      passive = true,
+      auto_delete = false,
+      durable = true,
+      ssl = false,
+      user = "guest",
+      password = "guest",
+  })
+  local c_args = {
+      role = "consumer",
+      queue = "work_q",
+      vhost = "workhost",
+      exchange = "work.pub",
+      durable = true,
+      passive = true,
+      ssl = false,
+      consumer_tag = "test_consumer_tag",
+      user = "read_user",
+      password = "read_user",
+      prefetch_count = 10,
+  }
+  test:isnt(ctx, nil, 'Ctx created')
+
+  local ok, err = ctx:connect("127.0.0.1", 5672)
+  test:ok(ok, "Connect status ok")
+  test:is(err, nil, "No error on connect")
+
+  ok, err = ctx:setup()
+  test:ok(ok, "Setup status ok")
+  test:is(err, nil, "No error on setup")
+
+  local wrk
+  wrk, err = get_worker(c_args)
+  test:ok(ok, "Consume worker creation ok")
+  wrk:consume()
+  fiber.sleep(5)
+  local res
+  res, err = helper.get_rmq_data()
+  test:ok(res, 'rmq data get ok')
+  test:is(#res.consumer_details, 1, 'consumer_details contains 1 element')
+  test:diag(res.consumer_details[1].consumer_tag)
+  test:ok((res.consumer_details[1].consumer_tag == "test_consumer_tag"), 'consumer_tag ok')
+  ok, err = wrk:close()
+  test:ok(ok, "Close consumer status ok")
+  test:is(err, nil, "No error on consumer close")
+
+  ok, err = ctx:teardown()
+  test:ok(ok, "Teardown status ok")
+  test:is(err, nil, "No error on teardown")
+
+  ok, err = ctx:close()
+  test:ok(ok, "Close status ok")
+  test:is(err, nil, "No error on close")
+end
+
 local test = tap.test('amqp')
-test:plan(2)
+test:plan(4)
 test:test('basic', test_basic)
 test:test('no_routing_key', test_no_routing_key)
+test:test('prefetch_count', test_prefetch_count)
+test:test('consumer_tag', test_consumer_tag)
 
 os.exit(test:check() == true and 0 or -1)
--- test/helpers/helper.lua
+++ test/helpers/helper.lua
@@ -1,46 +1,60 @@
 local amqp = require('amqp')
 local fiber = require('fiber')
+local json = require('json')
 
-    local function consume_callback_wrapper(ch)
-        return function(payload)
-            ch:put(payload)
-        end
+local function consume_callback_wrapper(ch)
+    return function(payload)
+        ch:put(payload)
     end
+end
 
-    local function get_worker(args)
-      local wrk = {
-        channel_out = fiber.channel(1),
-      }
-      local w_ok, w_err
-      wrk.ctx = amqp.new({
-          role = "consumer",
-          queue = args.queue,
-          virtual_host = args.vhost,
-          exchange = args.exchange,
-          routing_key = args.routing_key,
-          ssl = false,
-          passive = args.passive,
-          durable = args.durable,
-          auto_delete = args.auto_delete,
-          user = args.user,
-          password = args.password,
-          callback = consume_callback_wrapper(wrk.channel_out),
-      })
-      w_ok, w_err = wrk.ctx:connect('127.0.0.1', 5672)
-      if not w_ok then return nil, w_err end
+local function get_worker(args)
+  local wrk = {
+    channel_out = fiber.channel(1),
+  }
+  local w_ok, w_err
+  wrk.ctx = amqp.new({
+      role = "consumer",
+      queue = args.queue,
+      virtual_host = args.vhost,
+      exchange = args.exchange,
+      routing_key = args.routing_key,
+      ssl = false,
+      passive = args.passive,
+      durable = args.durable,
+      auto_delete = args.auto_delete,
+      user = args.user,
+      password = args.password,
+      prefetch_count = args.prefetch_count,
+      consumer_tag = args.consumer_tag,
+      callback = consume_callback_wrapper(wrk.channel_out),
+  })
+  w_ok, w_err = wrk.ctx:connect('127.0.0.1', 5672)
+  if not w_ok then return nil, w_err end
 
-      wrk.consume = function(self)
-        self.consume_fiber = fiber.create(self.ctx.consume, self.ctx)
-      end
+  wrk.consume = function(self)
+    self.consume_fiber = fiber.create(self.ctx.consume, self.ctx)
+  end
 
-      wrk.close = function(self)
-        local ok, err = self.ctx:close()
-        self.consume_fiber:cancel()
-        return ok, err
-      end
-      return wrk
-    end
+  wrk.close = function(self)
+    local ok, err = self.ctx:close()
+    if self.consume_fiber:status() ~= 'dead' then self.consume_fiber:cancel() end
+    return ok, err
+  end
+  return wrk
+end
+
+local function get_rmq_data()
+  local hc = require('http.client')
+  local auth = 'Authorization: Basic Z3Vlc3Q6Z3Vlc3Q='
+  local resp = hc.get('http://localhost:15672/api/queues/workhost/work_q',
+    {headers = {Authorization='Basic Z3Vlc3Q6Z3Vlc3Q='}}
+  )
+  if resp.status ~= 200 then return nil, resp.status end
+  return json.decode(resp.body)
+end
 
 return {
     get_worker = get_worker,
+    get_rmq_data = get_rmq_data,
 }
